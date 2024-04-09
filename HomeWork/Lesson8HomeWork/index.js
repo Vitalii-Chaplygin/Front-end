45c4082120712e253c33b0ed71996d64d33d305a
Vitalii <chapliginvitaliy@gmail.com>
2024-04-09 22:08:03 +0200

    1

--- HomeWork/Lesson8HomeWork/index.js
+++ HomeWork/Lesson8HomeWork/index.js
@@ -24,8 +24,7 @@ if (userName === user2Name) {
 
 let numberArrey = [3, 45, 66, 19, 6, 89];
 for (let i = 0; i < numberArrey.length; i++) {}
-console.log(
-  "Элементы с индексами 0,2,4 = [" +numberArrey[0] +"," +numberArrey[2] +"," + numberArrey[4]+"]");
+console.log("Элементы с индексами 0,2,4 = [" +numberArrey[0] +"," +numberArrey[2] +"," + numberArrey[4]+"]");
 
 //===================================================================================
 
