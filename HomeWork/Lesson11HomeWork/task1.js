1f31a2c8ce609229cffe702cb8c8226cd8a2c780
Vitalii <chapliginvitaliy@gmail.com>
2024-04-15 19:45:58 +0200

    2

--- HomeWork/Lesson11HomeWork/task1.js
+++ HomeWork/Lesson11HomeWork/task1.js
@@ -1,22 +1,24 @@
 // Задача1:
 // Есть массив строк. Вывести каждую строку в div и вывести в документ
+const colorText = () => {
+  const arrString = [
+    "Name: Cristiano",
+    "Last name: Ronaldo",
+    "Age: 39",
+    "Football player",
+  ];
+  for (let i = 0; i < arrString.length; i++) {
+    const divTask1 = document.createElement("div");
+    const p = document.createElement("p");
 
-const arrString = [
-  "Name: Cristiano",
-  "Last name: Ronaldo",
-  "Age: 39",
-  "Football player",
-];
-for (let i = 0; i < arrString.length; i++) {
-  const divTask1 = document.createElement("div");
-  const p = document.createElement("p");
+    p.textContent = arrString[i];
+    p.style.fontSize = "30px";
+    divTask1.style.marginLeft = "10px";
+    divTask1.id = "divId_task1";
 
-  p.textContent = arrString[i];
-  p.style.fontSize = "30px";
-  divTask1.style.marginLeft="10px"
-  divTask1.id = "divId_task1";
+    divTask1.appendChild(p);
 
-  divTask1.appendChild(p);
-
-  document.body.appendChild(divTask1);
-}
+    document.body.appendChild(divTask1);
+  }
+};
+colorText();
---
+++ HomeWork/Lesson12HomeWork/index.js
@@ -0,0 +1,18 @@
+// Задача1:
+// создать форму состоящую из логин, пароль и повторите пароль и кнопки отправить
+// при нажатии на кнопку отправить производится праверка - если пароль и повторите пароль совпадают - тогда пользователю выводится сообщение: данные отправлены
+// иначе выводится сообщение пароль и повторите пароль не совпадают...
+
+const functionForme = () => {
+  const form1 = document.forms.formName;
+  const passwordValue = form1.elements.password.value;
+  const repeatPasswordValue = form1.elements.RepeatPassword.value;
+
+  if (passwordValue=== repeatPasswordValue) {
+    alert("данные отправлены");
+  } else {
+    alert("повторите ввод, пароли не совпадают");
+  }
+};
+document.getElementById("formId").addEventListener("submit", functionForme);
+
