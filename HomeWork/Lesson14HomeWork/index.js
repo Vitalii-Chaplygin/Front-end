d3cad59941004457a7738073c51efeba6d9e649f
Vitalii <chapliginvitaliy@gmail.com>
2024-04-18 01:14:58 +0200

    1

--- HomeWork/Lesson14HomeWork/index.js
+++ HomeWork/Lesson14HomeWork/index.js
@@ -1,19 +1,49 @@
 // Задание
-// Предположим вы получили объект с данными 
-// (создайте переменную, в которой будет храниться объект) - spaceship, в котором находятся характеристики космического корабля, 
+// Предположим вы получили объект с данными
+// (создайте переменную, в которой будет храниться объект) - spaceship, в котором находятся характеристики космического корабля,
 // такие как:name
 // manufacturer
 // crew
 // maxSpeed
 // Необходимо отобразить их на странице.
 // Стилизация на ваше усмотрение
-// Дополнительная задача: Создайте кнопку "Hide Manufacturer", 
+// Дополнительная задача: Создайте кнопку "Hide Manufacturer",
 // которая будет скрывать manufacturer корабля на странице
 
-const spaceship={
-  name: "mand",
-  manufacturer: "ferwer",
-  crew: "ewrew",
-  maxSpeed: 1000,
+const outputOnDisplay = () => {
+  const spaceship = {
+    name: "Orion",
+    manufacturer: "	Lockheed Martin Airbus Defence and Space Arianespace",
+    crew: { ShipCrew: ["Villi", "Tilli", "Billi", "Killi"] },
+    Speed: 10000,
+  };
 
+  const divName = document.getElementById("nameId");
+  divName.innerText = "name = " + spaceship.name;
+
+
+  const manufacturerName = document.getElementById("manufacturerId");
+  manufacturerName.innerText = "Manufacturer = " + spaceship.manufacturer;
+
+
+  const crew1 = document.getElementById("crewId");
+  const crewArray = spaceship.crew.ShipCrew;
+  for (i = 0; i < crewArray.length; i++);
+  crew1.innerText=crewArray[i];
+
+
+  const divSpeed = document.getElementById("maxSpeedId");
+  divSpeed.innerText = "name = " + spaceship.Speed+"km";
 };
+
+outputOnDisplay();
+
+const hideManufacturer = ()=>{
+  const buttonClear = document.getElementById('btn')
+buttonClear.addEventListener('click',()=>{
+  const manufacturerName = document.getElementById("manufacturerId");
+  manufacturerName.style.display = "none";
+
+});
+}
+hideManufacturer();
