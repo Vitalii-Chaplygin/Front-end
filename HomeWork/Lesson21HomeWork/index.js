ee31588b03936adfaf2f63895e432e7daa46bf74
Vitalii <chapliginvitaliy@gmail.com>
2024-04-28 22:12:37 +0200

    1

---
+++ HomeWork/Lesson21HomeWork/index.js
@@ -0,0 +1,22 @@
+// Создайте функцию delay, которая принимает число (value) и возвращает промис.
+// Промис должен разрешиться через 5 секунд и вернуть удвоенное значение аргумента value (в resolve).
+// Если аргумент не является числом, промис должен быть отклонен с текстом ошибки "The argument is not a number" (в reject)
+
+const delay = (value) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof value !== "number") {
+        reject("The argument is not a number");
+      } else {
+        resolve(value * 2);
+      }
+    }, 5000);
+  });
+};
+delay(4+'string')
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
