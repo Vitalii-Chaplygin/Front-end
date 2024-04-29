e2401cb726ecaaece21c5e4fc26a60ab562dab62
Vitalii <chapliginvitaliy@gmail.com>
2024-04-29 09:32:10 +0200

    1

---
+++ интернет магазин на чистм js/index.js
@@ -0,0 +1,24 @@
+const app = document.getElementById('app')
+
+
+// Nav
+const nav = document.createElement('div');
+
+// Main.
+const main = document.createElement('div');
+// вывели на страницу с помощью добавления 
+app.appendChild(nav)
+nav.className = 'nav'
+app.appendChild(main)
+main.className='main'
+
+
+nav.textContent = 'navigation'
+
+// создаем карточку товара
+let card = document.createElement('div')
+card.className ='card'
+main.appendChild(card)
+
+
+
