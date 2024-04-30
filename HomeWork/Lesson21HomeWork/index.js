1ec7122516eaf1e3eb1255c019d86a6855b59563
Vitalii <chapliginvitaliy@gmail.com>
2024-04-30 17:53:09 +0200

    1

--- HomeWork/Lesson21HomeWork/index.js
+++ HomeWork/Lesson21HomeWork/index.js
@@ -2,18 +2,18 @@
 // Промис должен разрешиться через 5 секунд и вернуть удвоенное значение аргумента value (в resolve).
 // Если аргумент не является числом, промис должен быть отклонен с текстом ошибки "The argument is not a number" (в reject)
 
-const delay = (value) => {
-  return new Promise((resolve, reject) => {
+const delay = (value) => 
+   new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (typeof value !== "number") {
+      if (typeof value !== 'number') {
         reject("The argument is not a number");
       } else {
         resolve(value * 2);
       }
     }, 5000);
   });
-};
-delay(4+'string')
+;
+delay(4+'4')
   .then((res) => {
     console.log(res);
   })
