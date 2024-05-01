6d6b8afcc2b330de0c112f0caf1f0a1202ee7dcd
Vitalii <chapliginvitaliy@gmail.com>
2024-05-01 22:39:31 +0200

    1

--- HomeWork/Lesson23HomeWork/index.js
+++ HomeWork/Lesson23HomeWork/index.js
@@ -1,23 +1,34 @@
 
 
-const btn = document.getElementById('btn');
-let urlGeorgiaUniversiti = "http://universities.hipolabs.com/search?country=Georgia";
-const listeorgiaUniversiti = () => 
+const btn = document.getElementById("btn");
+const app = document.getElementById("app");
+
+const urlGeorgiaUniversiti =
+  "http://universities.hipolabs.com/search?country=Georgia";
+
+const listeorgiaUniversiti = () => {
   fetch(urlGeorgiaUniversiti)
     .then((response) => {
-      if(!response.ok){
-        throw new Error('Ошибка загрузки'+ response.status)
+      if (!response.ok) {
+        throw new Error("Ошибка загрузки" + response.status);
       }
       return response.json();
     })
     .then((data) => {
-      data.forEach(item => {
-        console.log(data[0]);
+      const title = document.createElement("h1");
+      title.textContent = "Университеты Грузии";
+      app.append(title);
+      const ul = document.createElement("ul");
+
+      data.forEach((univer) => {
+        const li = document.createElement("li");
+        li.textContent = univer.name;
+        ul.appendChild(li);
       });
-      
-    });
 
-btn.addEventListener('click',listeorgiaUniversiti);
+      app.appendChild(ul);
+    })
+    
+};
 
-
-
+btn.addEventListener("click", listeorgiaUniversiti);
